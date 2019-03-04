bbd1bc924700e395854640307acda4030acff456
nidhin mahesh <nidhinmahesh1@gmail.com>
2019-03-04 17:45:56 +0530

    new: code to fetch weather api for a city

---
+++ fetchwebapi.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+type people struct {
+	//Number int `json:"number"`/
+
+}
+
+func main() {
+
+	url := ""
+
+	spaceClient := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "soil-moisture-control")
+
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	cityname := "name"
+	jsonErr := json.Unmarshal(body, &cityname)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	fmt.Println(cityname)
+}
